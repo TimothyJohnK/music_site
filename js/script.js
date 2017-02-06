ae62057455ca1263e63ba41e0e6b3e967b327a7e
TimothyJohnK <Tim@VectorLab.com>
2017-02-06 15:39:00 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    var lessons = document.querySelector('#lessons_front_page')
+    var composition = document.querySelector('#composition_front_page')
+    var calendar = document.querySelector('#calendar_front_page')
+    var show = document.querySelector('#my_header')
+    var newLessons
+    var newComposition
+    var newCalendar
+
+    show.onclick = function() {
+      document.querySelector('#body-wrapperID').style.display = 'block'
+      document.querySelector('#lessons_front_page2').style.display = 'none'
+      document.querySelector('#composition_front_page2').style.display = 'none'
+    }
+
+    lessons.onclick = function() {
+      document.querySelector('#body-wrapperID').style.display = 'none'
+      document.querySelector('#lessons_front_page2').style.display = 'block'
+      document.querySelector('#bottom_padding_container').style.display = 'block'
+    }
+
+    composition.onclick = function() {
+      document.querySelector('#body-wrapperID').style.display = 'none'
+      document.querySelector('#composition_front_page2').style.display = 'block'
+      document.querySelector('#bottom_padding_container').style.display = 'block'
+    }
+
+
+  } //end of window.onload wrapper
