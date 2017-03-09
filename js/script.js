a5d0d1bd029e03608328f911b93c8c335983e181
TimothyJohnK <Tim@VectorLab.com>
2017-03-08 22:46:29 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,31 +5,44 @@ window.onload = function() {
     var calendar = document.querySelector('#calendar_front_page');
     var show = document.querySelector('#my_header');
     var menu = document.querySelector('#menu-button');
-    var newLessons
-    var newComposition
-    var newCalendar
-    
-    menu.onclick = function() {
-      document.querySelector('#menu-contents').style.display = 'block';
+    var newLessons;
+    var newComposition;
+    var newCalendar;
+    var menuContents = document.querySelector('#menu-contents');
+    var spacing = document.querySelector('#navbar')
 
-    }
+    menu.onclick = function() {
+      if (menuContents.style.display === 'none') {
+          menuContents.style.display = 'block';
+          spacing.style.margin = '5.4em 1em 0 1em';
+      } else {
+        menuContents.style.display = 'none';
+        spacing.style.margin = '0em 1em';
+
+      };
+    };
+    
+
+
+
+
     show.onclick = function() {
       document.querySelector('#body-wrapperID').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'none';
-    }
+    };
 
     lessons.onclick = function() {
       document.querySelector('#body-wrapperID').style.display = 'none';
       document.querySelector('#lessons_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
-    }
+    };
 
     composition.onclick = function() {
       document.querySelector('#body-wrapperID').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
-    }
+    };
 
    
 }; //end of window.onload wrapper
