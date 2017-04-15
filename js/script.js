bdb54ff498c1c4969c47562525469493f9753e53
TimothyJohnK <Tim@VectorLab.com>
2017-04-15 01:00:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,48 @@ window.onload = function() {
     var spacing = document.querySelector('#navbar');
     var menuLessons = document.querySelector('#menu-Lessons');
     var menuComposition = document.querySelector('#menu-Composition');
-    var menuCalendar = document.querySelector('#menu-Calendar')
+    var menuCalendar = document.querySelector('#menu-Calendar');
+    var copyright = document.getElementById('#copyright');
 
+    var cal_month = document.getElementById("#cal_month");
+
+    var dateCnstnt = new Date();
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var monthLength = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    var calTable = document.getElementById("#calendarBody")
+  
+    
+    var currentMonthLength = monthLength[dateCnstnt.getMonth()];
+    var day = dateCnstnt.getDate();
+    var dayOfWeek = dateCnstnt.getDay();
+    var dayOfWeekCounter = dayOfWeek;
+    var numberOfWeek;
+    var activeCell;
+    
+    while (day > 0) {
+          numberOfWeek = Math.floor(((day-1) + dayOfWeek)/ 7);
+         
+          activeCell = calTable.insertRow(numberOfWeek);
+          var currentDayInFunc = activeCell.insertCell(dayOfWeek);
+          currentDayInFunc.innerText = day;
+          day--;
+          dayOfWeekCounter--;
+          if (dayOfWeekCounter < 0) {
+            dayOfWeekCounter = 6;
+          }
+    }
+
+ // second function that finishes the calendar by counting up from the current day
+
+    // while (day <= currentMonthLength) {
+
+
+    // for (var i=0; i >= monthLength[dateCnstnt.getMonth()]; i++) {
+    //     for (var j=dateCnstnt.getDay)
+
+
+    // };
 
     menu.onclick = function() {
       if (menuContents.style.display === 'none') {
@@ -25,22 +65,23 @@ window.onload = function() {
       };
     };
     
+    menu.addEventListener('click', e => console.log('why does it not open on first click?'));
 
     show.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'block';
+      document.querySelector('#main_page').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'none';
     };
 
     lessons.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#lessons_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#composition_front_page2').style.display = 'none';
       document.querySelector('#calendar_front_page2').style.display = 'none';
     };
     menuLessons.onclick = function(){
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#lessons_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#composition_front_page2').style.display = 'none';
@@ -48,14 +89,14 @@ window.onload = function() {
     };
 
     composition.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';
       document.querySelector('#calendar_front_page2').style.display = 'none';
     };
     menuComposition.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';  
@@ -63,18 +104,18 @@ window.onload = function() {
     };
 
     calendar.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#calendar_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';
       document.querySelector('#composition_front_page2').style.display = 'none';
     };
     menuCalendar.onclick = function() {
-      document.querySelector('#body-wrapperID').style.display = 'none';
+      document.querySelector('#main_page').style.display = 'none';
       document.querySelector('#calendar_front_page2').style.display = 'block';
       document.querySelector('#bottom_padding_container').style.display = 'block';
       document.querySelector('#lessons_front_page2').style.display = 'none';
-      document.querySelector('#composition_front_page2').style.display = 'none';  
+      document.querySelector('#composition_front_page2').style.display = 'none';
     };
 
 }; //end of window.onload wrapper
